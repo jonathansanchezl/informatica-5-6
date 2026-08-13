8e13906457151d5269a029190e7ac6011046bc9e
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-08-13 15:41:20 +0000

    story yell

--- story.py
+++ story.py
@@ -10,15 +10,22 @@ def main():
 #  print("hello", end=" ")
  #  print(planet)
 
-    name = input("what is your name? ")
-    color = input("name a color: ")
-    adjetive = input("Give me an adjetive: ")
-    goal = input("A goal you would like to achieve: ")
+    name = input("what is your name? ").strip().title()
+    color = input("name a color: ").strip().lower()
+    adjetive = input("Give me an adjetive: ").strip().lower()
+    goal = input("A goal you would like to achieve: ").strip().lower()
 
     print("Hello,", name)
 
     print("This is your story:")
     print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today I will finally {goal}.")
+
+
+    print("This is your story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today I will finally {goal}.")
+
+    yell=(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today I will finally {goal}.")
+    print(yell.upper())
 
 
 if __name__ == "__main__":
