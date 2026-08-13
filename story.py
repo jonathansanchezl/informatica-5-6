f42ebc9324313787e24c7e1b0ee43de0972ba328
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-08-13 15:08:18 +0000

    story

---
+++ story.py
@@ -0,0 +1,26 @@
+def main():
+#  planet = input("planet:")
+#  #separation
+#  print("Hello", planet)
+#  #concatenation
+#  print("hello " + planet)
+# #formatted strings
+#  print(f"hello {planet}")
+# #ending
+#  print("hello", end=" ")
+ #  print(planet)
+
+    name = input("what is your name? ")
+    color = input("name a color: ")
+    adjetive = input("Give me an adjetive: ")
+    goal = input("A goal you would like to achieve: ")
+
+    print("Hello,", name)
+
+    print("This is your story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today I will finally {goal}.")
+
+
+if __name__ == "__main__":
+ main()
+
