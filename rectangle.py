541010ed0f8f3ec17c817522d9b395bc6d94bfbd
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-08-17 15:00:17 +0000

    rectangle

---
+++ rectangle.py
@@ -0,0 +1,24 @@
+def main():
+
+    x = int(input("Enter the width for the rectangle: "))
+    y = "O"
+
+
+    print(x * y)
+    print(x * y)
+    print(x * y)
+    print(x * y)
+    print(x * y)
+
+    per = 5 + 5 + x + x
+    ar = 5 * x
+    d =  25 + x**2
+    dd = d*0.5
+    print(f"perimeter: {per}" )
+    print(f"area: {ar}" )
+    print(f"diagonal: {dd}" )
+
+if __name__ == "__main__":
+     main()
+
+
