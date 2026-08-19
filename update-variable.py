fd58873cef6ec46494102bbbebc2e5aaa27df73f
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-08-19 18:00:06 +0000

    update variable

---
+++ update-variable.py
@@ -0,0 +1,30 @@
+def main()
+# Augmented Assigment Operator
+    score=10
+    score += 10
+    print(score)
+
+    #substraction assigment operator
+    #x-= y
+    score = 100
+    score -= 15
+    print(score)
+
+    #multiplication assigment operator
+    score = 10
+    score *= 4
+    print(score)
+
+    #Division assigment operator
+    x = 10
+    x /= 5
+    print(x)
+
+    #modulus assigmnet operator
+    a = 10
+    b = 4
+    a %= b
+    print(a)
+
+if __name__ == "__main__"":
+    main()
