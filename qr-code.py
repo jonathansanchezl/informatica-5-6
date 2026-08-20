ed9b598b3ea5b274f9e3119533ae40f601e11342
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-08-20 15:36:23 +0000

    qrcode

---
+++ qr-code.py
@@ -0,0 +1,14 @@
+import qrcode
+def main():
+
+    song = "https://open.spotify.com/artist/7qqOVKujL6LlOzfiHsSn9Y"
+    qr = qrcode.QRCode(version=1, box_size = 5, border = 5)
+
+    qr.add_data(song)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("youtube-qr.png")
+
+if __name__ == "__main__":
+     main()
