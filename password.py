daad23956fbbe65174bb37722d25ab5b141b3df7
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-08-24 14:51:12 +0000

    password

---
+++ password.py
@@ -0,0 +1,12 @@
+def main():
+  attempt = int(input("Enter your guess: "))
+  if attempt == 1:
+    print("Yous succesfully knew the password")
+
+  print("the program ended")
+
+
+
+
+if __name__ == "__main__":
+     main()
