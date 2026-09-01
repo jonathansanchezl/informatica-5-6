bcd35caec634c3a107efc2629b8b058deaeeb62c
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-09-01 21:49:23 +0000

    coinflip

--- coinflip.py
+++ coinflip.py
@@ -1,14 +1,17 @@
 import random
-guess = int(input("heads or tails: "))
-value = random.randint(1, 2)
-if value == 1:
-    print("heads")
-elif value == 2:
-    print("tails")
-else:
-    print("enter a valid value")
-if guess == value:
-    print("You win!")
-else:
-    print("You lose :c")
-
+def main():
+    options = ["heads", "tails"]
+    attempts = 3
+    while attempts > 0:
+        value = random.choice(options)
+        guess = input("heads or tails?: ").strip().lower()
+        print("the coin landed on", value)
+        if guess == value:
+            print("winner")
+            break
+        else:
+            print("looser")
+            attempts -=1
+            print("attempts left:", attempts)
+if __name__ == "__main__":
+    main()
