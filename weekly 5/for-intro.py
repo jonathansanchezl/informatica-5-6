f1d31b0a97dc09e8cad3595e06870007d15fecd3
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-09-18 20:20:51 +0000

    week5

---
+++ weekly 5/for-intro.py
@@ -0,0 +1,9 @@
+def main():
+    doctrine = ["Faith","repentance","baptism","Holy Ghost","Endure until end"]
+    num = 1
+    for elements in doctrine:
+        print(f"{num}:{elements}")
+        num += 1
+
+if __name__ == "__main__":
+    main()
