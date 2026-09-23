ae7d8e6313c4e535df70cef71588fd135d0a124e
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-09-23 17:41:51 +0000

    tabless

--- tables.py
+++ tables.py
@@ -1,12 +1,20 @@
 def main():
     while True:
         number = int(input("Enter a number (1-10): "))
-        numbers = [1,2,3,4,5,6,7,8,9,10]
-        for i in numbers:
-             number2 = number*i
-             print(f"1 times {number} is {number2}")
-        if number <=10:
-                if number > 0:
+        if number <= 10:
+             if number > 0:
+                numbers = [1,2,3,4,5,6,7,8,9,10]
+                for i in numbers:
+                    number2 = number*i
+                    print(f"1 times {number} is {number2}")
+        exit = input("Do you want another or exit? ").strip().lower()
+        if exit == "another":
+            print("")
+        elif exit == "exit":
+            break
+        else:
+            print("type what you want to do between another or exit")
+
 
 
 if __name__ == "__main__":
